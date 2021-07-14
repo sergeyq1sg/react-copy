3f7bddfbbbb7e96f134a41febb2a070d19433978
maxq1seg <maximb00131516@gmail.com>
2021-07-14 08:23:28 +0300

    added async function feature

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 for (let item of "ma") {
   console.log(item);
 }
+let a = 8;
+console.log(a + 10);
+
+async function f() {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("готово!"), 1000);
+  });
+  let result = await promise; // будет ждать, пока промис не выполнится (*)
+  alert(result); // "готово!"
+}
+
+f();
